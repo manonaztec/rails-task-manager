df3eb822cfe8e9e7a921b1d2efe0895c4c39e78f
manon chauvin <manonlz.chauvin@gmail.com>
2017-05-09 17:06:55 +0200

    resolve rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:name, :done)
+  end
+end
